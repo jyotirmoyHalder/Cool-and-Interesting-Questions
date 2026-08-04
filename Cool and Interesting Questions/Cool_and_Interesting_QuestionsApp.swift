8cb203d571ded008374f106b167c101504698a4f
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2026-08-04 23:11:24 +0600

    Initial Commit

---
+++ Cool and Interesting Questions/Cool_and_Interesting_QuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cool_and_Interesting_QuestionsApp.swift
+//  Cool and Interesting Questions
+//
+//  Created by jyotirmoy_halder on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Cool_and_Interesting_QuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
